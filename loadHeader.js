01eb4b49c4ca9b2de208d0f605beca0d9e43d9be
caron adam <adam.caron80@gmail.com>
2025-03-14 12:02:40 +0100

    Update loadHeader.js

--- loadHeader.js
+++ loadHeader.js
@@ -2,12 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch("header.html")
         .then(response => {
             if (!response.ok) {
-                throw new Error("Erreur lors du chargement du header.");
+                throw new Error(`HTTP error! Status: ${response.status}`);
             }
             return response.text();
         })
         .then(data => {
             document.getElementById("header").innerHTML = data;
         })
-        .catch(error => console.error("Erreur lors du chargement du header:", error));
+        .catch(error => {
+            console.error("Erreur lors du chargement du header :", error);
+        });
 });
